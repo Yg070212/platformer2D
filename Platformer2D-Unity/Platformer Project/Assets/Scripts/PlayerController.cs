b5a86b0f02dc6bba6b8f46866a123b234a280c12
Yg070212 <winter070212@gmail.com>
2024-05-11 17:55:40 +0900

    (2024-5. 11) 플레이어 애니메이션, 타일맵 구현

--- Platformer2D-Unity/Platformer Project/Assets/Scripts/PlayerController.cs
+++ Platformer2D-Unity/Platformer Project/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.TextCore;
 
 // 접근 지정자 public private protected
 public class PlayerController : MonoBehaviour
@@ -21,11 +23,22 @@ public class PlayerController : MonoBehaviour
 
     [Header("점프")]
     public bool isGrounded;          // true : 캐릭터가 점프 할 수 있는 상태, false : 점프 못함
-    public float groundDistance = 2f;
+    public float groundDistance = 2f;   
     public LayerMask groundLayer;
+
+    [Header("Flip")]
+    public SpriteRenderer spriteRenderer;
+    private bool facingRight = true;
+    private int facingDirection = 1;
+
+    [Header("Animation")]
+    public Animator animator;
+    private bool isMove;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+
         rigidbody2D = GetComponent<Rigidbody2D>();
             
         Debug.Log("Hello Unity");
@@ -34,26 +47,87 @@ public class PlayerController : MonoBehaviour
 
         // 현재 내 위치를 startTransform으로 변경
         transform.position = startTransform.position;
-
-        
     }
 
+    void InitializePlayerStatus()
+    {
+        rigidbody2D.velocity = Vector3.zero;
+        facingRight = true;
+        spriteRenderer.flipX = false;
+        transform.position = startTransform.position;
+    }
     // Update is called once per frame
     // 1 프레임 마다 호출된다. - 반복적으로 실행 
     void Update()
     {
+        //함수 이름 앞에 마우스커서를 두고 Ctlr + R + R
+        HandleAnimation();
+        CollisionCheck();
+        HandleInput();
+        HandleFlip();
+        Move();
+
+
+        FallDownCheck();
+    }
+
+    private void FallDownCheck()
+    {
+        // y의 높이가 특정 지점보다 낮을때 낙사한 것으로 간주한다. =>
+        if (transform.position.y < -11)
+            InitializePlayerStatus();
+    }
+
+    private void HandleFlip()
+    {
+        
+    }
+
+    private void HandleAnimation()
+    {
+        // rigidbody.velocity : 현재 rigidbody 속도 = 0 움직이지 않는 상태, != 0 움직이고 있는 상태
+        isMove = rigidbody2D.velocity.x != 0;
+        animator.SetBool("isMove", isMove);
+        animator.SetBool("isGrounded", isGrounded);
+        // SetFloat 함수에 의해서 y최대일 때 1로 변환.. y 최소일 때 -1로 변환
+        //점프 키를 누르면. 순간적으로 y높이 증가, 중력에 의해
+        animator.SetFloat("yBelocity", rigidbody2D.velocity.y);
+    }
+
+    private void CollisionCheck()
+    {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer);
+    }
 
-        // 플레이어의 입력 값을 받아와야 합니다.
-
+    private void HandleInput()
+    {
         moveInput = Input.GetAxis("Horizontal");
-        rigidbody2D.velocity = new Vector2(moveSpeed * moveInput, rigidbody2D.velocity.y);
 
+        JumpButton();
+    }
+
+    private void JumpButton()
+    {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
-            // 점프 : Y Position _ rigidbody Y velocity를 점프 파워만큼 올려주면된다.
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
+            Jump();
         }
+    }
+
+    private void Jump()
+    {
+        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
+    }
+
+
+    private void Move()
+    {
+        rigidbody2D.velocity = new Vector2(moveSpeed * moveInput, rigidbody2D.velocity.y);
+    }
+
+    private void ColliderCheck()
+    {
+
     }
 
     private void OnDrawGizmos()
