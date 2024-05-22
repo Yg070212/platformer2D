979f909c834dc5580c610379b619ec7bcdeddda5
Yg070212 <winter070212@gmail.com>
2024-05-22 23:43:44 +0900

    복습

---
+++ Platformer Project/Assets/Scripts/ComponentTest/ComTest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEngine;
+
+public class ComTest : MonoBehaviour
+{
+    // 코드의 흐름
+    public GameObject TestObject;
+    // nullRefence에러. 변수에 데이터가 없어서 발생하는 에러다.
+    // 생성(초기화) 이벤트 함수에 데이터 변수를 할당하는 작업.
+    // Awake -> OnEnavle -> Start
+
+
+    private void Awake()
+    {
+        Debug.Log("Awake 실행");
+        TestObject = new GameObject();
+    }
+
+
+    private void Start()
+    {
+        Debug.Log("Start 실행");
+        TestObject.SetActive(false);
+    }
+
+
+    private void OnEnable()
+    {
+        Debug.Log("OneEnable 실행");
+    }
+
+
+}
