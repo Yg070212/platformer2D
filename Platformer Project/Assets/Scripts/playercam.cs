a600ffe9f32a26e4c9d9225b2f0e971de42cbc79
Yg070212 <winter070212@gmail.com>
2024-05-25 17:53:17 +0900

    수업

--- Platformer Project/Assets/Scripts/playercam.cs
+++ Platformer Project/Assets/Scripts/playercam.cs
@@ -9,6 +9,7 @@ public class playercam : MonoBehaviour
     Vector3 offset;                           // 카메라와 플레이어의 위치 차이
     public Transform playerTransform;         // 플레이어의 현재 위치 (플레이어가 움직일 때 변경되고, 카메라가 해당 위치를 차이만큼 쫓아감)
     public float fixedYPosition;              // 카메라의 Y 위치를 고정시키기 위한 기준값
+    [Range(0f, 1f)]                           // 아래 변수의 크기를 한정하는 문법. <- C# Attribute
     public float smoothValue;                 // 카메라의 선형 보간(부드러운 움직임을 위해) 두 위치 사이에 어느 정도 percent 이동할 건지..
 
     // Start is called before the first frame update
@@ -34,9 +35,9 @@ public class playercam : MonoBehaviour
 
         Vector3 targetPosition = playerTransform.position + offset;  // 벡터의 합 연산으로 카메라의 위치를 구한다.
         targetPosition.y = fixedYPosition;                           // 카메라의 Y(높이)는 고정시킨다.0
-        Vector3 smoothPosition
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothValue); 
         // 벡터의 합                                                 // 실제로 플레이어의 x방향으로만 따라다니고, Y는 고정시킨 값으로 카메락를 이동시킨다.
-        transform.position = targetPosition;
+        transform.position = smoothPosition;
         
     }
 }
